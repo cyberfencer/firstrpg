4548d3aa35b93ced90d418d114f885d4f10fa35d
cyberfencer <tboekbinder27@bfhsla.org>
2024-10-31 12:24:09 -0500

    sprinting

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
 
-
 @export var speed = 200
 @onready var animated_sprite = $AnimatedSprite2D
 var new_direction: Vector2 = Vector2.ZERO
 var animation: String
-var is_attacking
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-
-
-
+var is_attacking = false
+var is_running = false
+var base_speed = speed
+var base_animation_speed = 1.0  # Default animation speed
+var run_animation_speed = 2.0   # Animation speed while running
 
 func _physics_process(delta):
-	
 	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	if abs(direction.x) == 1 and abs(direction.y) == 1:
 		direction = direction.normalized()
-		
 	
 	var movement = direction * speed * delta
-	if is_attacking == false:
+	if not is_attacking:
 		move_and_collide(movement)
 		playerDirection(direction)
 
@@ -34,7 +31,6 @@ func playerDirection(direction: Vector2):
 	else:
 		animation = "idle_" + returnedDirection(direction)
 		animated_sprite.play(animation)
-
 
 func returnedDirection(direction: Vector2):
 	var normalized_direction = direction.normalized()
@@ -50,8 +46,7 @@ func returnedDirection(direction: Vector2):
 	elif normalized_direction.x < 0:
 		animated_sprite.flip_h = true
 		return "side"
-		
-		
+	
 	return default_return
 
 func _input(event):
@@ -59,10 +54,25 @@ func _input(event):
 		is_attacking = true
 		animation = "attack_" + returnedDirection(new_direction)
 		animated_sprite.play(animation)
-		
-
-
-
+	elif event.is_action_pressed("sprint"):
+		run()
+	elif event.is_action_released("sprint"):
+		stop_running()
 
 func _on_animated_sprite_2d_animation_finished():
 	is_attacking = false
+
+func run():
+	if not is_running:
+		is_running = true
+		speed = base_speed * 2
+		animated_sprite.animation_speed = run_animation_speed
+		print("Running: Speed =", speed, "Animation Speed =", animated_sprite.animation_speed)
+
+func stop_running():
+	if is_running:
+		is_running = false
+		speed = base_speed
+		animated_sprite.animation_speed = base_animation_speed
+		print("Stopped Running: Speed =", speed, "Animation Speed =", animated_sprite.animation_speed)
+
